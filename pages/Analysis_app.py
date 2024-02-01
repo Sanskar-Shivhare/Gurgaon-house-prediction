9d7919c8f12f168f03876c38d72a9325939be230
Sanskar Shivhare <110665348+Sanskar-Shivhare@users.noreply.github.com>
2024-02-01 17:49:41 +0530

    Add files via upload

---
+++ pages/Analysis_app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.figure_factory as ff
+
+base = 'dark'
+
+
+st.set_page_config(page_title="Analytics",page_icon='📊')
+
+st.title(":red[Analytics Module] 📈📊")
+st.image('images\hari.png',width=300)
+
+df = pd.read_csv('datasets\data_viz1.csv')
+
+group_df = df.groupby('sector')[['price','price_per_sqft','built_up_area','latitude','longitude']].mean()
+
+fig = px.scatter_mapbox(group_df, lat="latitude", lon="longitude", color="price_per_sqft", size='built_up_area',
+                  color_continuous_scale=px.colors.cyclical.IceFire, zoom=10,
+                  mapbox_style="open-street-map",width=1200,height=700,hover_name=group_df.index)
+
+st.header(':orange[map of gurgaon on the basis of area per sqft]')
+st.plotly_chart(fig,use_container_width=True)
+
+
+
+st.header(':orange[Area vs Price]')
+property_type = st.selectbox('Property Type',['flat','house'])
+if property_type=='flat':
+    fig1 = px.scatter(df[df['property_type']=='flat'], x="built_up_area", y="price", color="bedRoom")
+
+    st.plotly_chart(fig1)
+else:
+    fig1 = px.scatter(df[df['property_type']=='house'], x="built_up_area", y="price", color="bedRoom")
+
+    st.plotly_chart(fig1)
+
+
+st.header(':orange[BHK Pie chart]')
+sectors = df['sector'].unique().tolist()
+sectors.insert(0,'Over all')
+BHKZ_type = st.selectbox('Select Sector',sectors)
+
+if BHKZ_type=='Over all':
+    fig3 = px.pie(df, names='bedRoom')
+    st.plotly_chart(fig3)
+else:
+    fig3 = px.pie(df[df['sector']==BHKZ_type], names='bedRoom')
+    st.plotly_chart(fig3)
+
+
+x1 = df[df['property_type'] == 'house']['price']
+x2 = df[df['property_type'] == 'flat']['price']
+st.header(':orange[distribution of houses and flats]')
+fig4 = ff.create_distplot([x1, x2], ['house', 'flats'], bin_size=.5,
+                         curve_type='kde'
+                         )
+
+st.plotly_chart(fig4)
---
+++ pages/Price_Predictor.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+import lzma
+
+st.set_page_config(page_title='Price Predictor',page_icon='🏡')
+
+
+# property_type	sector	bedRoom	bathroom	balcony	agePossession	built_up_area	servant room	
+# store room	furnishing_type	luxury_category	floor_category
+with open('df.pkl','rb') as file:
+    df = pickle.load(file)
+
+
+# Specify the path to your compressed pipeline pickle file
+compressed_pipeline_file_path = 'ml_pipeline_optimized_custom.pkl.xz'
+
+# Open the compressed pipeline pickle file for reading in binary mode ('rb')
+with lzma.open(compressed_pipeline_file_path, 'rb') as file:
+    # Load the pipeline from the compressed pickle file
+    pipeline = pickle.load(file)
+
+
+st.title(':red[Price Predictor]')
+st.image('images\predictor.png',width=300)
+
+st.header(':blue[Enter your Inputs]')
+# property_type input
+property_type = st.selectbox('Property Type',['flat','house'])
+
+# sector
+sector = st.selectbox('Sector',sorted(df['sector'].unique().tolist()))
+
+# bedroom
+bedroom = float(st.selectbox('Number of Bedrooms',sorted(df['bedRoom'].unique().tolist())))
+# bathroom
+bathroom = float(st.selectbox('Number of Bathrooms',sorted(df['bathroom'].unique().tolist())))
+
+# Balcony
+balcony = st.selectbox('Number of Balconies',sorted(df['balcony'].unique().tolist()))
+
+# agePossession
+property_age = st.selectbox('Property Age',sorted(df['agePossession'].unique().tolist()))
+
+# buituparea
+built_up_area = float(st.number_input('Built up area'))
+
+# servant room
+if st.selectbox('Servant room',['Yes','No']) == 'Yes':
+    servant_room = 1.0
+else:
+    servant_room = 0.0
+
+# store room
+if st.selectbox('Store room',['Yes','No']) =='Yes':
+    store_room = 1.
+else:
+    store_room = 0.
+
+
+# furnishing type
+furnishing_type =  st.selectbox('Furnishing Type',sorted(df['furnishing_type'].unique().tolist()))
+    
+
+# luxury_category
+luxury_category = st.selectbox('Luxury Type',sorted(df['luxury_category'].unique().tolist()))
+
+# floor_category
+floor_category = st.selectbox('Floor Category',sorted(df['floor_category'].unique().tolist()))
+
+
+
+if st.button('predict'):
+    # make a dataframe
+    data = [[property_type, sector, bedroom, bathroom, balcony, property_age, built_up_area, servant_room, store_room, furnishing_type, luxury_category, floor_category]]
+    columns = ['property_type', 'sector', 'bedRoom', 'bathroom', 'balcony',
+       'agePossession', 'built_up_area', 'servant room', 'store room',
+       'furnishing_type', 'luxury_category', 'floor_category']
+
+    # Convert to DataFrame
+    one_df = pd.DataFrame(data, columns=columns)
+
+    # predict
+    result = np.expm1(pipeline.predict(one_df))[0]
+    low = result-0.22
+    high = result+0.22
+
+    # display
+
+    st.text(f'The price of the {property_type} is between {np.round(low,2)} cr and {np.round(high,2)} cr')
+    st.balloons()
+    st.snow()
+
+
+    
